e23e4aaee68e0880a898b910a2b1beb3e41e3b96
Artur Smirnov <artursmirnovserg@gmail.com>
2022-05-12 22:44:54 +0300

    S8T13L3

--- cpp-s8/main.cpp
+++ cpp-s8/main.cpp
@@ -1,11 +1,13 @@
 ﻿#include "T12L6.h"
 #include "log_duration.h"
 #include "T13L2.h"
+#include "T13L3.h"
 
 using namespace std;
 
 
 int main() {
 	//Test1();
-	TestLowerBound();
+	//TestLowerBound();
+	TestExploreKeyWords();
 }
